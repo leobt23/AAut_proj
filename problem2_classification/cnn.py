d337c75d4771104a7a5f496f4eed496716b19636
Leo <leo.amadobrito32@gmail.com>
2022-11-01 15:30:23 +0000

    .

--- problem2_classification/cnn.py
+++ problem2_classification/cnn.py
@@ -1,4 +1,3 @@
-from statistics import mode
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,14 +8,11 @@ from tensorflow.python import keras
 from keras.utils import to_categorical
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import EarlyStopping
-from sklearn.model_selection import KFold
 from sklearn.model_selection import train_test_split
-import keras.backend as K
 from imblearn.over_sampling import SMOTE
 from tensorflow import keras
 from keras.optimizers import Adam
 from sklearn.metrics import balanced_accuracy_score, accuracy_score
-
 
 # Step 1 - Get data
 def data() -> np.array:
@@ -25,17 +21,19 @@ def data() -> np.array:
     X_test = np.load("problem2_classification/Xtest_Classification2.npy")
     return X, y, X_test
 
+#TODO check others methods
 def smote(X: np.array, y: np.array) -> np.array:
     X, y = SMOTE().fit_resample(X, y)
     return X, y
 
-def scale_data(X: np.array) -> np.array:
-    X = np.reshape(X, (121314, 5, 5, 3)) # 8k images 30x30 with 3 colours
-    # Scale data
+def scale_data(X: np.array, n_patches: int) -> np.array:
+    X = np.reshape(X, (n_patches, 5, 5, 3)) #  5x5 with 3 colours
     #Change pixels to probabilities between 0 and 1
     X = X / 255
+    return X
 
-
+#TODO check others methods
+#TODO How can this be configured
 def augmentation(X: np.array) -> np.array:
 #Data augmentation
     datagen = ImageDataGenerator(
@@ -52,41 +50,44 @@ def augmentation(X: np.array) -> np.array:
     datagen.fit(X)
     return X, datagen
 
+#TODO - Is this to do? Check with Guilherme
 def y_hot_encoding(y: np.array) -> np.array:
     # One hot encoding class variables
     # Otherwise, our machine learning algorithm won’t be able to directly take in that as input.
     y = to_categorical(y)
     return y
 
+# Check patience
 def earlystopping():
     #Stop training when accuracy has stopped improving
     callback = EarlyStopping(monitor='loss', patience=8, restore_best_weights=True)
     return callback
 
 def construct_and_fit(X, y, callback, datagen):
-    # For now, data will be split in this form. After must be used KFold(5) 
-    # 80% train and 20% test 
-    X_train_split, X_test_split, y_train_split, y_test_split = train_test_split(X, y, test_size=0.20)
-    #Model architecture
+
+    X_train_split, X_test_split, y_train_split, y_test_split = train_test_split(X, y, test_size=0.20)    
+
     model = Sequential()
 
-    # Convolution - Transform image map in a smaller one
-    model.add(Convolution2D(32, (3, 3), activation='relu', input_shape=(5, 5, 3)))
-    #For Spatial Invariance
-    model.add(MaxPooling2D(pool_size=(2,2)))
-    model.add(Convolution2D(128, (3, 3),activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2,2)))
-    model.add(Convolution2D(128, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2,2)))
-    model.add(Dropout(0.25))
+    model.add(Convolution2D(16, (2, 2), activation='relu', input_shape=(5, 5, 3)))
+    model.add(MaxPooling2D(pool_size=(1,1)))
+    model.add(Convolution2D(32,(2, 2), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(1,1)))
+    model.add(Convolution2D(64, (2, 2), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(1,1)))
+    # TODO - Is this dropout necessary
+#    model.add(Dropout(0.25))
 
     model.add(Flatten())
-    model.add(Dense(128, activation='relu'))
-    model.add(Dropout(0.25))
+    model.add(Dense(64, activation='relu'))
+    # TODO - Is this dropout necessary
+ #   model.add(Dropout(0.25))
     model.add(Dense(3, activation='softmax'))
 
     #Compile model
-    #ADAM OU SGD
+    #ADAM ou Gradient descent?
+    #Change metric
+    # binary_crossentropy or categorical_crossentropy is the best loss metric for multicalss classification?
     model.compile(loss='binary_crossentropy',  
                 optimizer = keras.optimizers.Adam(learning_rate=0.001),           
                 metrics=['accuracy'])
@@ -95,17 +96,10 @@ def construct_and_fit(X, y, callback, datagen):
         
     #Fit model on training data
 
-    hist = model.fit(datagen.flow(X_train_split, y_train_split, batch_size=64, 
+    hist = model.fit(datagen.flow(X_train_split, y_train_split, batch_size=8, 
     subset='training'),validation_data=datagen.flow(X_test_split, y_test_split, 
-    batch_size=32, subset='validation'), epochs=10, callbacks=callback)
+    batch_size=8, subset='validation'), epochs=10, callbacks=callback)
 
-    #Fit model on training data
-
-    """
-    hist = model.fit(datagen.flow(X_train, y_train, batch_size=64, 
-    subset='training'),validation_data=datagen.flow(X_test, y_test, 
-    batch_size=32, subset='validation'), epochs=20, callbacks=callback)
-    """
     return model, hist
 
 #y_predicted = model.predict()
@@ -114,6 +108,7 @@ def construct_and_fit(X, y, callback, datagen):
 
 #Visualize the models accuracy
 
+#TODO - Change this params
 def ploting(hist):
     plt.plot(hist.history['custom_f1'])
     #plt.plot(hist.history['val_custom_f1'])
@@ -134,20 +129,14 @@ def ploting(hist):
 
 #balanced_accuracy_score(y_val_real, final_predictions_val)
 #accuracy_score(y_val_real, final_predictions_val)
-"""
-X_test_class1 = np.load("Xtest_Classification1.npy")
-X_test_class2 = np.reshape(X_test_class1, (1367, 30, 30, 3)) 
-predictionss = model.predict(X_test_class2)
-predictionss = np.rint(predictionss)
-np.save('y.npy', predictionss)
-y1 = np.load("y.npy")
-y_df123 = pd.DataFrame(y1)
-df_final = y_df123.drop(0, axis=1)
 
-np_final = df_final.to_numpy()
+def save_results(model, X_test):
+    X_test = np.reshape(X_test, (1367, 5, 5, 3)) 
+    prediction = model.predict(X_test)
+    #TODO - Check this rint
+    prediction = np.rint(prediction)
+    np.save('y.npy', prediction)
 
-np.save('y_final.npy', np_final)
-"""
 
 
 
@@ -155,7 +144,7 @@ def main():
     ### Train
     X, y, X_test = data()
     X, y = smote(X, y)
-    X = scale_data(X)
+    X = scale_data(X, n_patches = 121314)
     X, datagen = augmentation(X)
     y = y_hot_encoding(y)
     callback = earlystopping()
