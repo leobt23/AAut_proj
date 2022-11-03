aa920e383391295b1f8f7f80dd3989cb94a4af9c
Leo <leo.amadobrito32@gmail.com>
2022-11-03 22:03:34 +0000

    .

--- problem2_classification/cnn.py
+++ problem2_classification/cnn.py
@@ -13,15 +13,51 @@ from tensorflow import keras
 from sklearn.metrics import balanced_accuracy_score
 from keras import layers
 
-# Step 1 - Get data
 def get_data() -> np.array:
+    """Function to get data from numpy file.
+
+    Returns:
+        np.array: Input data for training.
+        np.array: Labels of data for training.
+        np.array: Input data for testing.
+    """
     X = np.load("problem2_classification/Xtrain_Classification2.npy")
     y = np.load("problem2_classification/ytrain_Classification2.npy")
     X_test = np.load("problem2_classification/Xtest_Classification2.npy")
+
     return X, y, X_test
 
+
+def split_data(X: np.array, y:np.array) -> np.array:
+    """Method to split data for testing and training. 
+
+    Args:
+        X (np.array): Input data.
+        y (np.array): Labels of data.
+
+    Returns:
+        np.array: X_train_split - Input data for training.
+        np.array: X_test_split - Input data for testing.
+        np.array: y_train_split - Labels for training.
+        np.array: y_test_split - Labels for testing.
+    """
+
+    X_train_split, X_test_split, y_train_split, y_test_split = train_test_split(X, y, test_size=0.20, random_state=42)  
+
+    return X_train_split, X_test_split, y_train_split, y_test_split 
+
 #TODO check others methods
-def smote(X: np.array, y: np.array) -> np.array:
+def oversampling_method(X: np.array, y: np.array) -> np.array:
+    """Method to do oversampling to the data.
+
+    Args:
+        X (np.array): Input data for training
+        y (np.array): Labels for training
+
+    Returns:
+        np.array: X - Data input oversampled.
+        np.array: y - Labels oversampled.
+    """
 
     X, y = SMOTE().fit_resample(X, y)
 
@@ -36,10 +72,7 @@ def scale_data(X_train: np.array, X_test: np.array, n_patches_train: int, n_patc
 
     return X_train, X_test
 
-def split_data(X,y):
-    X_train_split, X_test_split, y_train_split, y_test_split = train_test_split(X, y, test_size=0.20, random_state=42)  
 
-    return X_train_split, X_test_split, y_train_split, y_test_split 
 
 """
 #TODO check others methods
@@ -167,7 +200,7 @@ def main():
     ### Train
     X, y, X_test = get_data()
     X_train_split, X_test_split, y_train_split, y_test_split = split_data(X,y)
-    X_train_split, y_train_split = smote(X_train_split, y_train_split) 
+    X_train_split, y_train_split = oversampling_method(X_train_split, y_train_split) 
     X_train_split, X_test_split = scale_data(X_train_split, X_test_split, n_patches_train = 96981, n_patches_test = 10140) # real number wtout altered data: 50700
     #X_train_split, datagen = augmentation(X_train_split)
     y_train_split, y_test_split = y_hot_encoding(y_train_split, y_test_split)
