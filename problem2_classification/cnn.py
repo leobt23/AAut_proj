1bdd9e1f866348ba33a90d17dd155797685b2e8b
Leo <leo.amadobrito32@gmail.com>
2022-11-03 21:00:54 +0000

    last_implementation

--- problem2_classification/cnn.py
+++ problem2_classification/cnn.py
@@ -2,20 +2,19 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Dense, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
 from tensorflow.python import keras 
 from keras.utils import to_categorical
-from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import EarlyStopping
 from sklearn.model_selection import train_test_split
 from imblearn.over_sampling import SMOTE
 from tensorflow import keras
-from keras.optimizers import Adam
-from sklearn.metrics import balanced_accuracy_score, accuracy_score
+from sklearn.metrics import balanced_accuracy_score
+from keras import layers
 
 # Step 1 - Get data
-def data() -> np.array:
+def get_data() -> np.array:
     X = np.load("problem2_classification/Xtrain_Classification2.npy")
     y = np.load("problem2_classification/ytrain_Classification2.npy")
     X_test = np.load("problem2_classification/Xtest_Classification2.npy")
@@ -23,15 +22,26 @@ def data() -> np.array:
 
 #TODO check others methods
 def smote(X: np.array, y: np.array) -> np.array:
+
     X, y = SMOTE().fit_resample(X, y)
+
     return X, y
 
-def scale_data(X: np.array, n_patches: int) -> np.array:
-    X = np.reshape(X, (n_patches, 5, 5, 3)) #  5x5 with 3 colours
-    #Change pixels to probabilities between 0 and 1
-    X = X / 255
-    return X
+def scale_data(X_train: np.array, X_test: np.array, n_patches_train: int, n_patches_test: int) -> np.array:
+    X_train = np.reshape(X_train, (n_patches_train, 5, 5, 3)) #  5x5 with 3 colours
+    X_train = X_train / 255.0
 
+    X_test = np.reshape(X_test, (n_patches_test, 5, 5, 3)) #  5x5 with 3 colours
+    X_test = X_test / 255.0
+
+    return X_train, X_test
+
+def split_data(X,y):
+    X_train_split, X_test_split, y_train_split, y_test_split = train_test_split(X, y, test_size=0.20, random_state=42)  
+
+    return X_train_split, X_test_split, y_train_split, y_test_split 
+
+"""
 #TODO check others methods
 #TODO How can this be configured
 def augmentation(X: np.array) -> np.array:
@@ -49,93 +59,105 @@ def augmentation(X: np.array) -> np.array:
 
     datagen.fit(X)
     return X, datagen
+    """
 
-#TODO - Is this to do? Check with Guilherme
-def y_hot_encoding(y: np.array) -> np.array:
-    # One hot encoding class variables
-    # Otherwise, our machine learning algorithm won’t be able to directly take in that as input.
-    y = to_categorical(y)
-    return y
+def y_hot_encoding(y_train_split: np.array, y_test_splity: np.array) -> np.array:
+    y_train_split = to_categorical(y_train_split)
+
+    y_test_splity = to_categorical(y_test_splity)
+
+    return y_train_split, y_test_splity
 
 # Check patience
 def earlystopping():
     #Stop training when accuracy has stopped improving
-    callback = EarlyStopping(monitor='val_loss', patience=8, restore_best_weights=True)
+    callback = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
     return callback
 
-def construct_and_fit(X, y, callback, datagen):
-
-    X_train_split, X_test_split, y_train_split, y_test_split = train_test_split(X, y, test_size=0.20)    
+def construct_and_fit(X_train_split, X_test_split, y_train_split, y_test_split, callback):
+    data_augmentation = keras.Sequential([
+        layers.RandomFlip("horizontal_and_vertical"),
+        layers.RandomRotation(0.1),
+        layers.RandomZoom(0.1),
+        ])
 
     model = Sequential()
 
-    model.add(Convolution2D(16, (2, 2), activation='relu', input_shape=(5, 5, 3)))
-    model.add(MaxPooling2D(pool_size=(1,1)))
-    model.add(Convolution2D(32,(2, 2), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(1,1)))
-    model.add(Convolution2D(64, (2, 2), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(1,1)))
-    # TODO - Is this dropout necessary
-#    model.add(Dropout(0.25))
+   # model.add(data_augmentation)
+    model.add(Convolution2D(32, (2, 2), padding='same', activation='relu', input_shape=(5, 5, 3)))
+    model.add(MaxPooling2D(pool_size=(2,2)))
 
     model.add(Flatten())
     model.add(Dense(64, activation='relu'))
-    # TODO - Is this dropout necessary
- #   model.add(Dropout(0.25))
+    model.add(Dense(64, activation='relu'))
     model.add(Dense(3, activation='softmax'))
 
-    #Compile model
-    #ADAM ou Gradient descent?
-    #Change metric
-    # binary_crossentropy or categorical_crossentropy is the best loss metric for multicalss classification?
     model.compile(loss='categorical_crossentropy',
                 optimizer = keras.optimizers.Adam(learning_rate=0.001),           
-                metrics=['balanced_accuracy_score'])
+                metrics=['accuracy'])
 
     model.summary()
         
     #Fit model on training data
 
-    hist = model.fit(datagen.flow(X_train_split, y_train_split, batch_size=8, 
-    subset='training'),validation_data=datagen.flow(X_test_split, y_test_split, 
-    batch_size=8, subset='validation'), epochs=10, callbacks=callback)
+    hist = model.fit(X_train_split, y_train_split,validation_data=(X_test_split, y_test_split), epochs=50, callbacks=callback)
 
     return model, hist
 
-#y_predicted = model.predict()
 
-#loss: 0.3230 - custom_f1: 0.8607 - val_loss: 0.3478 - val_custom_f1: 0.8348
+#Balanced_accuracy_score: 0.8450650814662192
 
 #Visualize the models accuracy
 
-"""
+
 #TODO - Change this params
 def ploting(hist):
-    plt.plot(hist.history['balanced_accuracy_score'])
-    #plt.plot(hist.history['val_custom_f1'])
+    plt.plot(hist.history['accuracy'])
+    plt.plot(hist.history['val_accuracy'])
     plt.title('Model balanced_accuracy_score')
-    plt.ylabel('custom_f1')
+    plt.ylabel('val_accuracy')
     plt.xlabel('Epoch')
     #plt.legend(['Train', 'Val'], loc = 'lower right')
     plt.show()
 
     plt.plot(hist.history['loss'])
-    #plt.plot(hist.history['val_loss'])
+    plt.plot(hist.history['val_loss'])
     plt.title('Model Loss')
     plt.ylabel('Loss')
     plt.xlabel('Epoch')
     #plt.legend(['Train', 'Val'], loc = 'upper right')
     plt.show()
-"""
+
 
 #balanced_accuracy_score(y_val_real, final_predictions_val)
 #accuracy_score(y_val_real, final_predictions_val)
 
-def save_results(model, X_test):
-    X_test = np.reshape(X_test, (1367, 5, 5, 3)) 
-    prediction = model.predict(X_test)
+def predict(model, X_test_split, y_test_split):
+    X_test = np.reshape(X_test_split, (10140, 5, 5, 3)) 
+    prediction = model.predict(X_test_split)
     #TODO - Check this rint
     prediction = np.rint(prediction)
+    df_predict = pd.DataFrame(prediction)
+    df_y_test = pd.DataFrame(y_test_split)
+    
+    df_predict = df_predict.values.argmax(axis=1)
+    df_y_test = df_y_test.values.argmax(axis=1)
+    
+    count2=0
+    count1=0
+    count0=0
+    for i in df_y_test:
+        if(i == 2):
+            count2 += 1
+        if(i == 1):
+            count1 += 1
+        if(i == 0):
+            count0 += 1
+
+    print(f'Balanced_accuracy_score: {balanced_accuracy_score(df_predict, df_y_test)}')
+    return prediction
+            
+def save_results(prediction: np.array):            
     np.save('y.npy', prediction)
 
 
@@ -143,13 +165,14 @@ def save_results(model, X_test):
 
 def main():
     ### Train
-    X, y, X_test = data()
-    X, y = smote(X, y)
-    X = scale_data(X, n_patches = 121314)
-    X, datagen = augmentation(X)
-    y = y_hot_encoding(y)
+    X, y, X_test = get_data()
+    X_train_split, X_test_split, y_train_split, y_test_split = split_data(X,y)
+    X_train_split, y_train_split = smote(X_train_split, y_train_split) 
+    X_train_split, X_test_split = scale_data(X_train_split, X_test_split, n_patches_train = 96981, n_patches_test = 10140) # real number wtout altered data: 50700
+    #X_train_split, datagen = augmentation(X_train_split)
+    y_train_split, y_test_split = y_hot_encoding(y_train_split, y_test_split)
     callback = earlystopping()
-    model, hist = construct_and_fit(X, y, callback, datagen)
+    model, hist = construct_and_fit(X_train_split, X_test_split, y_train_split, y_test_split, callback)
     ploting(hist)
 
 
@@ -158,7 +181,7 @@ def main():
     # Normalize data and reshape
 
     # Predict 
-
+    prediction = predict(model, X_test_split, y_test_split)
     # Choose best value line by line
 
     # Save results
