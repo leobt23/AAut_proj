f4275f758c814e3857e0625a2d941f80e8ddecb6
EconPmor <moreira.pedro@gmail.com>
2022-11-08 21:10:42 +0000

    adicionei f1

--- problem1_classification/knn.py
+++ problem1_classification/knn.py
@@ -5,12 +5,14 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import KFold
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
+from sklearn.metrics import f1_score
+
 
 
 # Step 1 - Get data
-X = np.load("Xtrain_Classification1.npy")
+X = np.load("problem1_classification/Xtrain_Classification1.npy")
 X_df = pd.DataFrame(X)
-y = np.load("ytrain_Classification1.npy")
+y = np.load("problem1_classification/ytrain_Classification1.npy")
 y_df = pd.DataFrame(y)
 
 X = np.reshape(X, (8273, 30, 30, 3)) # 8k images 30x30 with 3 colours
@@ -31,5 +33,7 @@ model = KNeighborsClassifier(n_neighbors=20, n_jobs=3)
 model.fit(X_train, y_train)
 y_pred = model.predict(X_validation)
 accuracy = accuracy_score(y_validation, y_pred)*100
+f1= f1_score(y_validation, y_pred)
 print(accuracy)
+print(f1)
 #print(classification_report(y_validation, model.predict(X_validation), target_names=le.classes_))
--- problem1_classification/report_analysis.py
+++ problem1_classification/report_analysis.py
@@ -1,6 +1,10 @@
 
 import numpy as np
 import pandas as pd
+#from imblearn.over_sampling import SMOTE
+from keras.utils import to_categorical
+
+
 
 # Step 1 - Get data
 X = np.load("problem1_classification/Xtrain_Classification1.npy")
@@ -13,3 +17,13 @@ X_test_df = pd.DataFrame(X_test)
 
 
 y_df.value_counts()
+data_ytrain = to_categorical(y)
+
+
+X, y = SMOTE().fit_resample(X, y)
+y_df = pd.DataFrame(y)
+print(y_df.value_counts())
+
+X = np.reshape(X, (10284, 30, 30, 3)) # 8k images 30x30 with 3 colours
+
+
